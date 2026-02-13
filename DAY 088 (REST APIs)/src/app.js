e306e57a4f46f7a6b23491b4269f8eca2ae0c09a
SouravPareek <souravpareek1510@gmail.com>
2026-02-13 19:30:13 +0530

    REST APIs understanding

--- DAY 088 (REST APIs)/src/app.js
+++ DAY 088 (REST APIs)/src/app.js
@@ -7,12 +7,13 @@ const express = require("express")
 
 const app = express()/*server create ho jata ha*/
 
+app.use(express.json())
 
 const notes = [
-    {
-        title : "test title 1",
-        description : "test description 1"
-    }
+    // {
+    //     title : "test title 1",
+    //     description : "test description 1"
+    // }
 ];
 
 app.get("/", (req, res)=>{
@@ -20,9 +21,30 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/notes", (req, res)=>{
-    console.log(req.body);
-    
+    console.log(req.body)
+    notes.push(req.body)
+
+    console.log(notes)
     res.send("note created")
 });
+
+
+app.get("/notes", (req, res)=>{
+    res.send(notes)
+});
+
+
+//without : req.params doesnt work
+app.delete("/notes/:index", (req, res)=>{
+    delete notes[req.params.index];
+
+    res.send(`note ${req.params.index} deleted successfully`)
+})
+
+app.patch("/notes/:index", (req, res)=>{
+    notes[req.params.index].description  = req.body.description
+
+    res.send("note updated successfully")
+})
 
 module.exports = app /*server export karna */
