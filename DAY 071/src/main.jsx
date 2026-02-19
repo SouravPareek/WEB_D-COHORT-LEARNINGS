3d6ed446e3854980bf1b1b3e32aba02a29648a16
SouravPareek <souravpareek1510@gmail.com>
2026-02-19 21:33:53 +0530

    removing error

--- DAY 071/src/main.jsx
+++ DAY 071/src/main.jsx
@@ -2,10 +2,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const main = () => {
-  return (
-    <div><App /></div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
