2c51788d569824c81c5a15c148054fc979aa4a47
SouravPareek <souravpareek1510@gmail.com>
2026-03-05 19:28:37 +0530

    Updated env

--- DAY 083_2(Context API Action)/src/App.jsx
+++ DAY 083_2(Context API Action)/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
-  return (
-    <div>App</div>
+  const getData = async ()=>{
+    const response = await axios.get('https://fakestoreapi.com/products')
+    console.log(response);
+    
+  }
+    return (
+    <div className='h-screen w-screen bg-zinc-600 text-white text-2xl p-5'>
+        <button className='rounded-2xl px-4 py-3 bg-red-500 active:scale-95' onClick={getData}>Get Data</button>
+    </div>
   )
 }
 
