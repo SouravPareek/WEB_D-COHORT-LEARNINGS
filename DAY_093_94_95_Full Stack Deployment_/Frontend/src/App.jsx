f8230fd5e8d8c19ff677e8d75955e6689aea0a6e
SouravPareek <souravpareek1510@gmail.com>
2026-03-24 09:31:55 +0530

    Changing connection from local host to render uri

--- DAY_093_94_95_Full Stack Deployment_/Frontend/src/App.jsx
+++ DAY_093_94_95_Full Stack Deployment_/Frontend/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
     console.log("integrating ");
 
     function fetchNotes() {
-        axios.get("http://localhost:3000/api/notes").then((res) => {
+        axios.get("https://web-d-cohort-learnings.onrender.com//api/notes").then((res) => {
             setnotes(res.data.notes);
         });
     }
     function postNote() {
         axios
-            .post("http://localhost:3000/api/notes", {
+            .post("https://web-d-cohort-learnings.onrender.com//api/notes", {
                 title: title,
                 description: desc,
             })
@@ -29,7 +29,7 @@ function App() {
     }
     function deleteNote(noteId) {
         axios
-            .delete("http://localhost:3000/api/notes/" + noteId)
+            .delete("https://web-d-cohort-learnings.onrender.com//api/notes/" + noteId)
             .then(() => {
                 fetchNotes();
             })
@@ -49,7 +49,7 @@ function App() {
         }
         
         axios
-        .patch(`http://localhost:3000/api/notes/${noteId}`, updatedFields)
+        .patch(`https://web-d-cohort-learnings.onrender.com//api/notes/${noteId}`, updatedFields)
         .then(() => {
             fetchNotes();
             setTitle("");
