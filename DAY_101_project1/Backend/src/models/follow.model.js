618da919fdb1d53afc6a315fab58c9722bde77ef
SouravPareek <souravpareek1510@gmail.com>
2026-04-09 05:48:43 +0530

    Backend almost completed

--- DAY_101_project1/Backend/src/models/follow.model.js
+++ DAY_101_project1/Backend/src/models/follow.model.js
@@ -9,6 +9,15 @@ const followSchema = new mongoose.Schema({
         type: String,
         required: [true, "Followee is required"]
     },
+    status:{
+        type: String,
+        default: "pending",
+        //enum=>what values status property can have here, if any other value is tried than database doesnt allow it.
+        enum: {
+            values: ["pending", "accepted", "rejected"],
+            message: "status can only be pending, accepted or rejected."
+        }
+    }
 }, {
     timestamps: true
 })
